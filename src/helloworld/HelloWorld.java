80f99d93086e019d672d1875d6c1ab768d7f6d97
Rodion <irodionzaytsev@gmail.com>
2019-10-04 14:30:45 +0300

    msg

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -26,7 +26,7 @@ public class HelloWorld extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().println("Hello, world!");
 	}
 
 	/**
